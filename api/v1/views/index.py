aff6e6397dc556e268ceb3199511b792404e4011
Ambogo2 <ruthambogo.ra@gmail.com>
2024-08-04 18:14:26 +0300

    commit

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,26 @@
 #!/usr/bin/python3
-"""Returns a json response"""
+"""creates flask app app_views"""
 
-from flask import jsonify
 from api.v1.views import app_views
+from flask import jsonify
 from models import storage
+
+@app_views.route('/status')
+def api_status():
+    """returns status code"""
+    response = {'status': 'OK'}
+    return jsonify(response)
+
+@app_views.route('/status', methods =['GET'])
+def object_stats():
+    """Retrieves the number of object by type"""
+    objects = {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
+    }
+    return jsonify(objects)
+    
